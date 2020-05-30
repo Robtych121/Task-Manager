a9ba7b47f37f02d258877bdc8de8ef5bec8709d0
Robert Davies <rob.davies1991@gmail.com>
2020-05-30 06:24:47 +0100

    added parent list to form template and styles

--- task_lists/views.py
+++ task_lists/views.py
@@ -10,13 +10,17 @@ def create_or_edit_task_list(request, pk=None):
     or edit a task list depending if the task list ID
     is null or not
     """
+    grouped_lists = Task_List.objects.filter(type='Group')
+
     task_list = get_object_or_404(Task_List, pk=pk) if pk else None
     if request.method == 'POST':
+        data = request.POST.copy()
         form = TaskListForm(request.POST, instance=task_list)
         if form.is_valid():
             task_list = form.save(commit=False)
+            task_list.parent_list = data.get('parent_list')
             task_list.save()
             return redirect('home')
     else:
         form = TaskListForm(instance=task_list)
-    return render(request, 'task_list_form.html', {'form': form})
+    return render(request, 'task_list_form.html', {'form': form, 'grouped_lists': grouped_lists})
