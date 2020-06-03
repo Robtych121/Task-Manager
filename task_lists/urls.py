1b5660729503420c643f976589bc10ae17dfdd32
Robert Davies <rob.davies1991@gmail.com>
2020-06-03 10:12:50 +0100

    added delete task list to urls

--- task_lists/urls.py
+++ task_lists/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import views
 from django.urls import path
-from .views import create_or_edit_task_list, view_list, create_new_task_post
+from .views import create_or_edit_task_list, view_list, create_new_task_post, delete_task_list_post
 
 urlpatterns = [
     path('view_list/<int:id>', view_list, name="view_list"),
     path('new_task_list/', create_or_edit_task_list, name="create_or_edit_task_list"),
-    path('new_task/', create_new_task_post, name="create_new_task_post")
+    path('new_task/', create_new_task_post, name="create_new_task_post"),
+    path('delete_list/<int:id>', delete_task_list_post, name="delete_task_list_post"),
 ]
