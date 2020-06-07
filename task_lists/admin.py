0e1b052c792700af1304ec939ba8acb26956a909
Robert Davies <rob.davies1991@gmail.com>
2020-06-07 12:01:10 +0100

    amend admin layout

--- task_lists/admin.py
+++ task_lists/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Task, Task_List, Task_List_Users
 
 # Register your models here.
+
+
+class TaskListUsersAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'list_id', 'perm_view', 'perm_add', 'perm_edit', 'perm_delete')
+
+
 admin.site.register(Task)
 admin.site.register(Task_List)
-admin.site.register(Task_List_Users)
+admin.site.register(Task_List_Users, TaskListUsersAdmin)
