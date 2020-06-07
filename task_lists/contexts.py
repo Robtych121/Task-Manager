affe11e27734f252ad841d24c37adf8f04193fcc
Robert Davies <rob.davies1991@gmail.com>
2020-06-07 11:57:47 +0100

    added permission filters to navigation

--- task_lists/contexts.py
+++ task_lists/contexts.py
@@ -1,9 +1,10 @@
-from .models import Task_List
+from .models import Task_List, Task_List_Users
 
 
 def get_task_lists(request):
     task_lists = Task_List.objects.exclude(parent_list__isnull=False).order_by('name')
-    return { 'nav_task_lists': task_lists}
+    task_list_users = Task_List_Users.objects.filter(user_id=request.user.id)
+    return { 'nav_task_lists': task_lists, 'task_list_users': task_list_users}
 
 
 def get_sub_task_lists(request):
