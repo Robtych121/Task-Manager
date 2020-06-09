31391238b70e823b05638d0b73b0b996d354ea28
Robert Davies <rob.davies1991@gmail.com>
2020-06-09 23:21:13 +0100

    consolidated urls

--- dj_taskmanager/urls.py
+++ dj_taskmanager/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
     path('accounts/', include('accounts.urls')),
     path('', include('task_lists.urls')),
 ]
--- task_lists/urls.py
+++ task_lists/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import views
 from django.urls import path
+from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
 from .views import create_or_edit_task_list, view_list, create_new_task_post, delete_task_list_post, manage_task_lists, delete_task_list_post_manage, edit_task_list, edit_task_list_manage, set_importance_from_list, set_completed_from_list, edit_task, view_task_list_users, edit_task_list_users, delete_task_list_user_post, add_task_list_user, set_list_owner
 
 urlpatterns = [
+    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
     path('manage_task_lists/', manage_task_lists, name="manage_task_lists"),
     path('view_list/<int:id>', view_list, name="view_list"),
     path('new_task_list/', create_or_edit_task_list, name="create_or_edit_task_list"),
